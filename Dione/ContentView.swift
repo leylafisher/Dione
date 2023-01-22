3a8ac0191f40228b1d1ec7391de3396a7cc06f04
Kevin Ahmad <kevinahmad.f@gmail.com>
2023-01-22 09:29:33 +0700

    Initial Commit

---
+++ Dione/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Dione
+//
+//  Created by Kevin ahmad on 22/01/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Dione/DioneApp.swift
@@ -0,0 +1,17 @@
+//
+//  DioneApp.swift
+//  Dione
+//
+//  Created by Kevin ahmad on 22/01/23.
+//
+
+import SwiftUI
+
+@main
+struct DioneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
